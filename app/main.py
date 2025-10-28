5e0efd465ecb373abb427708dadfbbba46d23f8b
Виктор Кочнев <105153978+laym0n@users.noreply.github.com>
2025-10-28 23:48:16 +0300

    try fix dast alerts

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from html import escape
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, PlainTextResponse
 from fastapi.templating import Jinja2Templates
@@ -5,20 +7,21 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request, q: str = ""):
-    # намеренно простая страница, отражающая ввод
-    # (для DAST это даст находки типа отражений/хедеров)
     return templates.TemplateResponse(
         "index.html",
-        {"request": request, "q": q}
+        {"request": request, "q": q},
     )
+
 
 @app.get("/healthz")
 def healthz():
     return PlainTextResponse("OK")
 
+
 @app.get("/echo", response_class=HTMLResponse)
 def echo(x: str = ""):
-    # намеренно без экранирования - упрощённая цель для ZAP
-    return HTMLResponse(f"<h1>ECHO</h1><div>you said: {x}</div>")
+    safe_body = "<h1>ECHO</h1><div>you said: {}</div>".format(escape(x, quote=True))
+    return HTMLResponse(safe_body)
